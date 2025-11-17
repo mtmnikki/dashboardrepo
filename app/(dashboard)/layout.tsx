17503ee0a2b7e45402d36fb11718776202584fae
mtmnikki <nikki@mtmtft.com>
2025-11-16 18:45:14 -0600

    Refactor layout to use SidebarProvider

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,9 +1,10 @@
 import { AppSidebar } from '@/components/app-sidebar'
 import { NavMain } from '@/components/nav-main'
-import { SidebarProvider, Sidebar, SidebarContent } from "@/components/ui/sidebar"
+import { SidebarProvider } from "@/components/ui/sidebar"
 import { ThemeProvider } from '@/components/theme-provider'
 import { data } from '@/components/sidebar-data'
 import { Toaster } from 'react-hot-toast'
+
 export default function DashboardLayout({
   children,
 }: {
@@ -16,19 +17,20 @@ export default function DashboardLayout({
       enableSystem
       disableTransitionOnChange
     >
-      <div className="flex h-screen overflow-hidden">
-        <AppSidebar >
-        <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-          <NavMain items={data.navMain} />
-          <main>
-            <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
-              {children}
-            </div>
-          </main>
+      <SidebarProvider>
+        <div className="flex h-screen overflow-hidden">
+          <AppSidebar/>
+          <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+            <NavMain items={data.navMain} />
+            <main>
+              <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+                {children}
+              </div>
+            </main>
+          </div>
         </div>
-        </AppSidebar>
-      </div>
-      <Toaster />
+        <Toaster />
+      </SidebarProvider>
     </ThemeProvider>
   )
 }
